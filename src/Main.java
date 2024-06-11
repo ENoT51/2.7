e3033d66a2a6de2a0736e6545e05dd77b0c22fa5
ENoT51 <ambarov515@gmail.com>
2024-06-11 21:59:37 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.*;
+
+
+public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+    public static void main(String[] args) {
+        }
+        public static void tusk1(){
+        for (int num : nums)
+            if (num % 2 != 0) {
+                
+            }
+
+    }
+    public static void tusk2(){
+
+    }
+}
+
